981fcf56521f4f39cdb21e6f12aaaa7af196ba44
GosuMarti <martiveselinov@gmail.com>
2023-12-06 13:24:40 +0200

    create entity class

--- src/main/java/com/web/app/springbootwebapp/Entity/Employee.java
+++ src/main/java/com/web/app/springbootwebapp/Entity/Employee.java
@@ -1,2 +1,59 @@
-package com.web.app.springbootwebapp.Entity;public class Employee {
+package com.web.app.springbootwebapp.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "tbl_employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String department;
+
+    public Employee(long id, String name, String email, String department) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.department = department;
+    }
+
+    public Employee(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
